52ca8abceffa985f05257ebd2691d55438e9fb30
leandrosardi <leandro.sardi@expandedventure.com>
2022-09-06 15:15:52 -0300

    leandrosardi/mysaas#102 leandrosardi/blackstack-deployer#14

--- extension.rb
+++ extension.rb
@@ -41,22 +41,24 @@ BlackStack::Extensions::add ({
             # back up old configuration file
             # setup new configuration file
             :command => "
-                source /home/%ssh_username%/.rvm/scripts/rvm; rvm install 3.1.2; rvm --default use 3.1.2 > /dev/null 2>&1;
-                cd /home/%ssh_username%/code/mysaas/extensions/leads/p > /dev/null 2>&1; 
-                export RUBYLIB=/home/%ssh_username%/code/mysaas > /dev/null 2>&1;
-                nohup ruby export.rb > /dev/null 2>&1 &
+                source /home/%ssh_username%/.rvm/scripts/rvm; rvm install 3.1.2; rvm --default use 3.1.2;
+                cd /home/%ssh_username%/code/mysaas/extensions/leads/p; 
+                export RUBYLIB=/home/%ssh_username%/code/mysaas;
+                nohup ruby export.rb;
             ",
             :sudo => false,
+            :background => true,
         }, {
             # back up old configuration file
             # setup new configuration file
             :command => "
-                source /home/%ssh_username%/.rvm/scripts/rvm; rvm install 3.1.2; rvm --default use 3.1.2 > /dev/null 2>&1;
-                cd /home/%ssh_username%/code/mysaas/extensions/leads/p > /dev/null 2>&1; 
-                export RUBYLIB=/home/%ssh_username%/code/mysaas > /dev/null 2>&1;
-                nohup ruby stat.search.rb > /dev/null 2>&1 &
+                source /home/%ssh_username%/.rvm/scripts/rvm; rvm install 3.1.2; rvm --default use 3.1.2;
+                cd /home/%ssh_username%/code/mysaas/extensions/leads/p; 
+                export RUBYLIB=/home/%ssh_username%/code/mysaas;
+                nohup ruby stat.search.rb;
             ",
             :sudo => false,
+            :background => true,
         }],
     }],
 =end
